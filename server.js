28da16423a9036e7143dab94ee0f134b77c22fe1
Bennie Kingwood <bennie.kingwood@gmail.com>
2014-01-17 18:13:09 -0800

    Changed the routing to point to the coming soon page. Moved the build folder over to the root url.

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var path      = require('path')
 
 // app configuration
 app.configure(function () {
+	app.use(express.logger('dev'));
   app.set('port', process.env.PORT || 8889);
-  app.use("/build", express.static(__dirname + '/build'));
-  app.use(express.logger('dev'));
+  app.use("/", express.static(__dirname + '/build'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -24,8 +24,8 @@ app.configure('production', function(){
 });
 
 // root route
-app.get('/app', function(req, res) {
-  res.redirect('/build/index.html');
+app.get('/', function(req, res) {
+  res.redirect('/coming.html');
 });
 
 console.log('ulink node server running on port ' + app.get('port'));
